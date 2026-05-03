dd956692a307e4c8cea962ea21081300fe2180b3
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-05-03 13:13:43 +0200

    Vor Entfernung {observe: 'response'}

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ionic: Schlagzeile von REST-API abrufen #
+
+<br>
+
+Diese Repository enthält eine Ionic/Angular-App, die demonstriert, wie man Daten von einer REST-API abruft.
+
+<br>
+
+----
+
+## License ##
+
+<br>
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License) for the files in this repository.
+
+<br>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
+
+interface SchlagzeilenItem {
+  schlagzeile: string;
+  inland     : boolean;
+  ort        : string;
+}
+
+
+interface SchlagzeilenAntwort {
+  items: SchlagzeilenItem[];
+  anzahl: number;
+}
+
+
+/**
+ * Für Verwendung HttpClient muss in app.module.ts die Methode provideHttpClient() als Provider
+ * registriert werden (siehe src/app/app.module.ts).
+ */
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +28,82 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+    /** URL für Abruf einer Schlagzeile von Web-API. 
+     * Doku: https://api.el-decker.de/badnews_doku.html 
+     *
+     * Beispiel für JSON-Antwort der Web-API:
+     * ``` 
+     * {
+     *   "items": [
+     *     {
+     *      "schlagzeile": "Weißwurst-Diebstahl in Bayern",
+     *      "inland"     : true,
+     *      "ort"        : "Bayern"
+     *    }
+     *   ],
+     *   "anzahl": 1
+     * }
+     * ```
+     */
+    readonly URL_WEBAPI = "https://api.el-decker.de/badnews.php?nur_inland=true";
 
+
+    /**
+     * Konstruktor für *Dependency Injection*.
+     */
+    constructor( private httpClient: HttpClient,
+                 private alertCtrl : AlertController ) {}
+
+
+    /**
+     * Event-Handler für Button, der Web-API-Request auslöst.
+     */
+    public onSchlagzeileLadenButton() {
+
+        const httpRequestObservable = 
+          this.httpClient.get<SchlagzeilenAntwort>( this.URL_WEBAPI );
+        
+        httpRequestObservable.subscribe({
+            next : (antwort) => this.verarbeiteHttpResponse( antwort ),
+            error: (fehler ) => this.verarbeiteHttpFehler(   fehler  )
+        });
+    }
+
+
+    /**
+     * Event-Handler für erfolgreiche HTTP-Response.
+     * 
+     * @param schlagzeilenAntwort Response-Body der Web-API, der die Schlagzeile enthält.
+     */
+    private verarbeiteHttpResponse( schlagzeilenAntwort: SchlagzeilenAntwort ) {
+
+      console.log( "HTTP-Response erhalten:", schlagzeilenAntwort ) ;
+
+      // get Schlagzeilen-Item aus Schlagzeilen-Antwort
+      const schlagzeilenItem = schlagzeilenAntwort.items[0];
+      
+      if ( schlagzeilenItem == null ) {
+        console.warn( "Keine Schlagzeilen-Item erhalten." );
+        return;
+      }
+    }
+
+
+    /**
+     * Event-Handler für Fehler bei HTTP-Request.
+     * 
+     * @param fehler Objekt mit Informationen zum Fehler 
+     */
+    private async verarbeiteHttpFehler( fehler: HttpErrorResponse ) {
+
+        console.error( "Fehler bei HTTP-Request:", fehler ) ;
+
+        const alert = await this.alertCtrl.create({
+            header: "Fehler",
+            message: "Beim Abruf der Schlagzeile ist ein Fehler aufgetreten: " + fehler.message,
+            buttons: ["OK"]
+          });
+
+        await alert.present();
+    }
 }
